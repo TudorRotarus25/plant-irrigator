292a750c790bf1edae5dc07280fe0a390344c29b
Tudor Rotarus <tudor.rotarus@skyscanner.net>
2024-09-15 17:27:42 +0100

    Add dummy on / off scripts

---
+++ off.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+
+in1 = 16
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(in1, GPIO.OUT)
+
+GPIO.output(in1, False)
+
+try:
+  GPIO.output(in1, False)
+except KeyboardInterrupt:
+  GPIO.cleanup()
